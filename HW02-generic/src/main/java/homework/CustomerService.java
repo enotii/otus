eb8f80ba752b2f7ea0be9a7393c0f972651ee436
Kristina <enotii@mail.ru>
2023-12-17 15:39:48 +0300

    fix after review

--- HW02-generic/src/main/java/homework/CustomerService.java
+++ HW02-generic/src/main/java/homework/CustomerService.java
@@ -3,27 +3,31 @@ package homework;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 import static java.util.Objects.nonNull;
 
 public class CustomerService {
-    TreeMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    NavigableMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
     private Map.Entry<Customer, String> entry = null;
 //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
-        Map.Entry<Customer, String> entry = map.firstEntry();
-        return nonNull(entry) ? Map.entry(new Customer(entry.getKey()), entry.getValue()) : null;
+        return getImmutableCopy(map.firstEntry());
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
         //return null; // это "заглушка, чтобы скомилировать"
     }
 
+
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        Map.Entry<Customer, String> entry = map.higherEntry(customer);
-        return nonNull(entry) ? Map.entry(new Customer(entry.getKey()), entry.getValue()) : null;
+        return getImmutableCopy(map.higherEntry(customer));
         //return null; // это "заглушка, чтобы скомилировать"
+    }
+
+    private Map.Entry<Customer, String> getImmutableCopy(Map.Entry<Customer, String> customerStringEntry) {
+        return nonNull(customerStringEntry) ? Map.entry(new Customer(customerStringEntry.getKey()), customerStringEntry.getValue()) : null;
     }
 
     public void add(Customer customer, String data) {
